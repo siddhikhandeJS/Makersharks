4f400e4144795cf8f44ef8aa937b5cd25fedabdd
siddhikhandeJS <95625943+siddhikhandeJS@users.noreply.github.com>
2024-08-23 14:21:54 +0530

    Update SupplierController.java

--- src/main/java/com/app/controller/SupplierController.java
+++ src/main/java/com/app/controller/SupplierController.java
@@ -2,16 +2,15 @@ package com.app.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.SupplierDto;
-import com.app.entities.Supplier;
+import com.app.entities.ManufacturingProcess;
+import com.app.entities.NatureOfBusiness;
 import com.app.service.SupplierService;
 
 @RestController
@@ -19,18 +18,20 @@ import com.app.service.SupplierService;
 public class SupplierController {
 	
 
-    private final SupplierService supplierService;
+	 private final SupplierService supplierService;
 
-    @Autowired
-    public SupplierController(SupplierService supplierService) {
-        this.supplierService = supplierService;
-    }
+	    @Autowired
+	    public SupplierController(SupplierService supplierService) {
+	        this.supplierService = supplierService;
+	    }
 
-    @PostMapping("/query")
-    public List<Supplier> querySuppliers(@Valid @RequestBody SupplierDto request) {
-        Supplier.NatureOfBusiness natureOfBusiness = Supplier.NatureOfBusiness.valueOf(request.getNatureOfBusiness().toUpperCase());
-        Supplier.ManufacturingProcess manufacturingProcess = Supplier.ManufacturingProcess.valueOf(request.getManufacturingProcess().toUpperCase());
-        return supplierService.querySuppliers(request.getLocation(), natureOfBusiness, manufacturingProcess, request.getLimit());
-    }
+	    @PostMapping("/query")
+	    public List<SupplierDto> querySuppliers(
+	            @RequestParam String location,
+	            @RequestParam NatureOfBusiness natureOfBusiness,
+	            @RequestParam ManufacturingProcess manufacturingProcess,
+	            @RequestParam(defaultValue = "10") int limit) {
+	        return supplierService.querySuppliers(location, natureOfBusiness, manufacturingProcess, limit);
+	    }
     
 }
